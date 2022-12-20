9e570da0bee7b44aceaced8c5ab9bb7b98068e14
Dominik Tränklein <dominik@bleech.de>
2022-12-20 14:08:30 +0100

    feat(ConnectionFailed): adjust error messages

--- components/metabox-classic-editor/metabox-notifications/metabox-notification-urls-mismatch.php
+++ components/metabox-classic-editor/metabox-notifications/metabox-notification-urls-mismatch.php
@@ -1,4 +1,4 @@
 <div class="vrts-metabox-notice vrts-metabox-notice-is-error">
 	<p><strong><?php esc_html_e( 'Connection failed', 'visual-regression-tests' ); ?></strong></p>
-	<p><?php esc_html_e( 'The URLs of your websites seemed to have changed since the activation of the plugin. Please restore the original urls or contact support for alternative solutions.', 'visual-regression-tests' ); ?></p>
+	<p><?php esc_html_e( 'Your website domain seemed to have changed since the plugin was activated. Please restore the original domain or uninstall and reinstall the plugin.', 'visual-regression-tests' ); ?></p>
 </div>
