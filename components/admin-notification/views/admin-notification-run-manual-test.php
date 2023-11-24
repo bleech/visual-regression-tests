95e8c819f3ecdd15bc1e5daf16d3699a046496a6
Harun Bašić <harun@bleech.de>
2023-11-24 14:19:28 +0100

    refactor(tests): change wording to run tests

--- components/admin-notification/views/admin-notification-run-manual-test.php
+++ components/admin-notification/views/admin-notification-run-manual-test.php
@@ -1,5 +1,5 @@
 <div class="vrts-notice notice notice-info is-dismissible" data-view="<?php echo esc_attr( $data['view'] ); ?>">
-	<h3><?php esc_html_e( 'Manual testing has started', 'visual-regression-tests' ); ?></h3>
-	<p><?php esc_html_e( 'Manual tests have started for all running tests.', 'visual-regression-tests' ); ?></p>
-	<p><?php esc_html_e( 'If the manual testing detects any visual differences, we will notify you via e-mail alerts.', 'visual-regression-tests' ); ?></p>
+	<h3><?php esc_html_e( 'Testing has started', 'visual-regression-tests' ); ?></h3>
+	<p><?php esc_html_e( 'Tests have started for all running tests.', 'visual-regression-tests' ); ?></p>
+	<p><?php esc_html_e( 'If the testing detects any visual differences, we will notify you via e-mail alerts.', 'visual-regression-tests' ); ?></p>
 </div>
