35697343b456d71fbabf085fc6262633782d5b78
Harun Bašić <harun@bleech.de>
2024-06-11 13:51:09 +0200

    feat(test-run-service): update finished run

--- includes/services/class-test-run-service.php
+++ includes/services/class-test-run-service.php
@@ -55,4 +55,45 @@ class Test_Run_Service {
 			return true;
 		}//end if
 	}
+
+	protected function update_finished_run( $data, $test_run ) {
+		$alert_ids = [];
+
+		foreach ($data['comparisons'] as $comparison) {
+			$test_id = $comparison['comparison_schedule_id'];
+			$alert_id = null;
+
+			if ( $comparison['pixels_diff'] > 1 && ! $comparison['matches_false_positive'] ) {
+				$post_id = Test::get_post_id_by_service_test_id( $test_id );
+				$alert_service = new Alert_Service();
+				$alert_id = $alert_service->create_alert_from_comparison( $post_id, $test_id, $comparison );
+				$alert_ids[] = $alert_id;
+			}//end if
+
+			$test_service = new Test_Service();
+			$test_service->update_test_from_comparison( $alert_id, $test_id, [
+				'comparison' => $comparison
+			] );
+		}
+		$update_data = [
+			'alerts' => maybe_serialize( $alert_ids ),
+			'started_at' => $data['started_at'],
+			'finished_at' => $data['finished_at'],
+			'is_running' => 0,
+		];
+		$tests = maybe_unserialize( $test_run->tests );
+		if (empty($tests)) {
+			$test_ids = array_map(function( $test ) {
+				return $test->id;
+			}, Test::get_by_service_test_ids($data['comparison_schedule_ids']) );
+			$update_data['tests'] = maybe_serialize( $test_ids );
+		}
+
+		Test_Run::save( $update_data, $test_run->id );
+
+		// TODO Send e-mail notification.
+		// $email_notifications = new Email_Notifications();
+		// $email_notifications->send_email( $comparison['pixels_diff'], $post_id, $alert_id );
+		return true;
+	}
 }
