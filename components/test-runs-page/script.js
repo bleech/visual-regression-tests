3b6462080c3f7b0ecb001ef7088dce1a3c7e60a4
Harun Bašić <harun@bleech.de>
2024-06-13 02:16:14 +0200

    fix: linting

--- components/test-runs-page/script.js
+++ components/test-runs-page/script.js
@@ -4,11 +4,11 @@ document
 		element.addEventListener( 'click', ( e ) => {
 			e.preventDefault();
 			const $row = element.closest( 'tr' );
-			const isHidden = $row.getAttribute( 'data-vrts-test-run-details' ) === 'hidden';
+			const isHidden =
+				$row.getAttribute( 'data-vrts-test-run-details' ) === 'hidden';
 			$row.setAttribute(
 				'data-vrts-test-run-details',
 				isHidden ? 'visible' : 'hidden'
 			);
-			console.log( 'Show test run details' );
 		} );
 	} );
