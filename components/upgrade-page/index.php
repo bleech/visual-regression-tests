3451dc42957477087f3031f7159eade948ebfa6a
Dominik Tränklein <dominik@bleech.de>
2024-10-02 13:01:02 +0200

    chore: replace embedded pricing url

--- components/upgrade-page/index.php
+++ components/upgrade-page/index.php
@@ -1,6 +1,6 @@
 <div class="wrap vrts_upgrade_page">
 	<iframe
-		src="https://bleech.de/en/vrts-upgrade/?current-tier=<?php echo rawurlencode( esc_attr( $data['tier_id'] ) ); ?>"
+		src="https://vrts.app/pricing-embedded/?current-tier=<?php echo rawurlencode( esc_attr( $data['tier_id'] ) ); ?>"
 		title="<?php echo esc_html( $data['title'] ); ?>"
 		loading="lazy"
 		width="100%"
