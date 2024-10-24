9e66d2def8a2b607d7c93d5fb35cbdc32f2975b9
Dominik Tränklein <dominik@bleech.de>
2024-10-24 10:10:41 +0200

    feat(Alerts): rename difference alt attribute

--- components/test-run-alerts/index.php
+++ components/test-run-alerts/index.php
@@ -54,7 +54,7 @@ $current_alert_id = isset( $data['alert']->id ) ? $data['alert']->id : 0;
 						data-vrts-state="<?php echo esc_attr( intval( $alert->alert_state ) === 0 ? 'unread' : 'read' ); ?>"
 						data-vrts-false-positive="<?php echo esc_attr( $alert->is_false_positive ? 'true' : 'false' ); ?>">
 						<figure class="vrts-test-run-alerts__card-figure">
-							<img class="vrts-test-run-alerts__card-image" src="<?php echo esc_url( Image_Helpers::get_screenshot_url( $alert, 'comparison', 'preview' ) ); ?>" alt="<?php esc_attr_e( 'Comparison Screenshot', 'visual-regression-tests' ); ?>">
+							<img class="vrts-test-run-alerts__card-image" src="<?php echo esc_url( Image_Helpers::get_screenshot_url( $alert, 'comparison', 'preview' ) ); ?>" alt="<?php esc_attr_e( 'Difference', 'visual-regression-tests' ); ?>">
 							<span class="vrts-test-run-alerts__card-flag"><?php vrts()->icon( 'flag' ); ?></span>
 						</figure>
 						<span class="vrts-test-run-alerts__card-title">
