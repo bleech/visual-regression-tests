eb19c463026c74e24f6a84182f4801d581862160
Harun Bašić <harun@bleech.de>
2024-10-29 11:10:33 +0100

    feat(TestRun): issue with container-type and fixed popup inside

--- components/comparisons/index.php
+++ components/comparisons/index.php
@@ -20,24 +20,26 @@ use Vrts\Core\Utilities\Image_Helpers;
 		</div>
 	</div>
 
-	<div class="vrts-comparisons__content">
-		<figure class="vrts-comparisons__figure" data-vrts-comparisons-slot="comparison">
-			<img class="vrts-comparisons__figure-image" <?php echo wp_kses_post( Image_Helpers::alert_image_hwstring( $data['alert'] ) ); ?> crossorigin="anonymous" src="<?php echo esc_url( Image_Helpers::get_screenshot_url( $data['alert'], 'comparison' ) ); ?>" alt="<?php esc_attr_e( 'Difference', 'visual-regression-tests' ); ?>" />
-			<span class="vrts-comparisons__slider-divider-clone"></span>
-		</figure>
-		<canvas class="vrts-comparisons__diff-inidicator" data-vrts-comparisons-diff-inidicator></canvas>
-		<div class="vrts-comparisons__slider" style="--vrts-comparisons-slider-aspect-ratio: <?php echo esc_attr( Image_Helpers::alert_image_aspect_ratio( $data['alert'] ) ); ?>">
-			<figure class="vrts-comparisons__figure" data-vrts-comparisons-slot="base">
-				<img class="vrts-comparisons__figure-image" <?php echo wp_kses_post( Image_Helpers::alert_image_hwstring( $data['alert'] ) ); ?> crossorigin="anonymous" src="<?php echo esc_url( Image_Helpers::get_screenshot_url( $data['alert'], 'base' ) ); ?>" alt="<?php esc_attr_e( 'Snapshot', 'visual-regression-tests' ); ?>" />
+	<div class="vrts-comparisons__content-container">
+		<div class="vrts-comparisons__content">
+			<figure class="vrts-comparisons__figure" data-vrts-comparisons-slot="comparison">
+				<img class="vrts-comparisons__figure-image" <?php echo wp_kses_post( Image_Helpers::alert_image_hwstring( $data['alert'] ) ); ?> crossorigin="anonymous" src="<?php echo esc_url( Image_Helpers::get_screenshot_url( $data['alert'], 'comparison' ) ); ?>" alt="<?php esc_attr_e( 'Difference', 'visual-regression-tests' ); ?>" />
+				<span class="vrts-comparisons__slider-divider-clone"></span>
 			</figure>
-			<figure class="vrts-comparisons__figure" data-vrts-comparisons-slot="target">
-				<img class="vrts-comparisons__figure-image" <?php echo wp_kses_post( Image_Helpers::alert_image_hwstring( $data['alert'] ) ); ?> crossorigin="anonymous" src="<?php echo esc_url( Image_Helpers::get_screenshot_url( $data['alert'], 'target' ) ); ?>" alt="<?php esc_attr_e( 'Screenshot', 'visual-regression-tests' ); ?>" />
-			</figure>
-			<span class="vrts-comparisons__slider-divider"></span>
-			<div class="vrts-comparisons__slider-handle">
-				<?php vrts()->icon( 'grip-dots' ); ?>
+			<canvas class="vrts-comparisons__diff-inidicator" data-vrts-comparisons-diff-inidicator></canvas>
+			<div class="vrts-comparisons__slider" style="--vrts-comparisons-slider-aspect-ratio: <?php echo esc_attr( Image_Helpers::alert_image_aspect_ratio( $data['alert'] ) ); ?>">
+				<figure class="vrts-comparisons__figure" data-vrts-comparisons-slot="base">
+					<img class="vrts-comparisons__figure-image" <?php echo wp_kses_post( Image_Helpers::alert_image_hwstring( $data['alert'] ) ); ?> crossorigin="anonymous" src="<?php echo esc_url( Image_Helpers::get_screenshot_url( $data['alert'], 'base' ) ); ?>" alt="<?php esc_attr_e( 'Snapshot', 'visual-regression-tests' ); ?>" />
+				</figure>
+				<figure class="vrts-comparisons__figure" data-vrts-comparisons-slot="target">
+					<img class="vrts-comparisons__figure-image" <?php echo wp_kses_post( Image_Helpers::alert_image_hwstring( $data['alert'] ) ); ?> crossorigin="anonymous" src="<?php echo esc_url( Image_Helpers::get_screenshot_url( $data['alert'], 'target' ) ); ?>" alt="<?php esc_attr_e( 'Screenshot', 'visual-regression-tests' ); ?>" />
+				</figure>
+				<span class="vrts-comparisons__slider-divider"></span>
+				<div class="vrts-comparisons__slider-handle">
+					<?php vrts()->icon( 'grip-dots' ); ?>
+				</div>
+				<input type="range" step="0.078125" class="vrts-comparisons__slider-control" data-vrts-comparisons-slider-control>
 			</div>
-			<input type="range" step="0.078125" class="vrts-comparisons__slider-control" data-vrts-comparisons-slider-control>
 		</div>
 	</div>
 	<span class="vrts-gradient-loader"></span>
