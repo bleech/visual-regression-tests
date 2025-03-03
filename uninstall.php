01b834e3090e6e9126fa8bb5b02954722f9002c0
Harun Bašić <harun@bleech.de>
2025-03-03 14:52:34 +0100

    feat(uninstall): add support for multisite

--- uninstall.php
+++ uninstall.php
@@ -24,15 +24,31 @@ require_once 'includes/tables/class-alerts-table.php';
 require_once 'includes/tables/class-tests-table.php';
 require_once 'includes/tables/class-test-runs-table.php';
 
-Vrts\Features\Admin_Notices::delete_dismissed_options();
-Vrts\Features\Metaboxes::delete_meta_keys();
-Vrts\Features\Service::disconnect_service();
-Vrts\Features\Service::delete_option();
-Vrts\Features\Subscription::delete_options();
-Vrts\Features\Cron_Jobs::remove_jobs();
-$vrts_manual_test_service = new Vrts\Services\Manual_Test_Service();
-$vrts_manual_test_service->delete_option();
+/**
+ * Uninstall.
+ */
+function vrts_uninstall() {
+	Vrts\Features\Admin_Notices::delete_dismissed_options();
+	Vrts\Features\Metaboxes::delete_meta_keys();
+	Vrts\Features\Service::disconnect_service();
+	Vrts\Features\Service::delete_option();
+	Vrts\Features\Subscription::delete_options();
+	Vrts\Features\Cron_Jobs::remove_jobs();
+	$vrts_manual_test_service = new Vrts\Services\Manual_Test_Service();
+	$vrts_manual_test_service->delete_option();
 
-Vrts\Tables\Alerts_Table::uninstall_table();
-Vrts\Tables\Tests_Table::uninstall_table();
-Vrts\Tables\Test_Runs_Table::uninstall_table();
+	Vrts\Tables\Alerts_Table::uninstall_table();
+	Vrts\Tables\Tests_Table::uninstall_table();
+	Vrts\Tables\Test_Runs_Table::uninstall_table();
+}
+
+if ( is_multisite() ) {
+	// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+	$vrts_blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+	foreach ( $vrts_blog_ids as $vrts_blog_id ) {
+		switch_to_blog( $vrts_blog_id );
+		vrts_uninstall();
+	}
+} else {
+	vrts_uninstall();
+}
